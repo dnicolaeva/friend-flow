ef7a3c037fcc5c9cf8a6db669f00259849c9dd69
Dana Nikolaeva Chambourova <dnicolaeva@hotmail.com>
2016-04-26 12:48:44 -0500

    Generated web interface / updated flask file structure / added upload file to server

--- flask_app/app/pages/views.py
+++ flask_app/app/pages/views.py
@@ -1,26 +1,53 @@
-from app.app_and_db import app, db, oauth
-from app.pages.models import City, Response
+from app.app_and_db import app
 from datetime import datetime
-from flask import jsonify, render_template, redirect, request, url_for
-
+from flask import jsonify, render_template, redirect, url_for, request
+from werkzeug import secure_filename
 import requests
+import dataloader
  
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['htm', 'html'])
+processed_json = {}
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
 @app.route('/')
 def index():
-  return render_template('pages/home_page.html')
+  # return render_template('pages/test_individual_graph.html')
+  return render_template('pages/default.html')
 
-@app.route('/api/getnoun/<string:input_phrase>/')
-def get_noun(input_phrase):
-  print(input_phrase)
-  api_response = 'hello'
-  return api_response
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+  file = request.files['file']
+  if file and allowed_file(file.filename):
+      global processed_json
+      processed_json = dataloader.load_friendship_json(file)
+      if processed_json is None:
+        print "Invalid file schema"
+        return redirect(url_for('index'))
+      else:
+        return render_template('pages/graph.html')
+  
+  elif file:
+  	print "Invalid file type"
+  	return redirect(url_for('index'))
 
+@app.route('/processed-json/', methods=['GET'])
+def echo():
+    global processed_json
+    return processed_json
+ 
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
-@app.route('/api/getnoun')
-def cities():
-  whoa = "whoa"
-  print whoa
-  return str(whoa)
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
